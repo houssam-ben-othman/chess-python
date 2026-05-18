419ac470d87b519643e93ad17c9327667f5206d2
Houssam BEN OTHMAN <houssam.ben_othman@edu.devinci.fr>
2026-05-18 18:31:19 +0200

    Ajout des déplacements de chacunes des pièces (sans prendre en compte la prise en diagonale du pion)

--- piece.py
+++ piece.py
@@ -6,20 +6,60 @@ class Piece :
         self.pos = new_pos
     
 class Pawn(Piece) :
-    pass
+    def move(self, new_pos) :
+        if self.color == "white" :
+            if ( self.pos[1]==1) :
+               if (new_pos[1] == self.pos[1] + 1 and new_pos[0] == self.pos[0] or (new_pos[1] == self.pos[1] + 2 and new_pos[0] == self.pos[0])) :
+                    self.pos = new_pos
+            else :
+                if (new_pos[1] == self.pos[1] + 1 and new_pos[0] == self.pos[0]) :
+                    self.pos = new_pos
+                    
+            
+        else :
+            if ( self.pos[1]==6) :
+               if (new_pos[1] == self.pos[1] - 1 and new_pos[0] == self.pos[0] or (new_pos[1] == self.pos[1] - 2 and new_pos[0] == self.pos[0])) :
+                    self.pos = new_pos
+            else :
+                if (new_pos[1] == self.pos[1] - 1 and new_pos[0] == self.pos[0]) :
+                    self.pos = new_pos
 
 class Rook(Piece) :
-    pass
+    def move(self, new_pos) :
+        if (new_pos[0] == self.pos[0] or new_pos[1] == self.pos[1]) :
+            self.pos = new_pos
 
 class Bishop(Piece) :
-    pass
+    def move(self, new_pos) :
+        for i in range(-7,8) :
+            if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i )or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
+                self.pos = new_pos
+                break
 
 class Queen(Piece) :
-    pass
+    def move(self, new_pos) :
+        if new_pos[0] == self.pos[0] or new_pos[1] == self.pos[1] :
+            self.pos = new_pos
+        else :
+            for i in range(-7,8) :
+                if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i )or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
+                    self.pos = new_pos
+                    break
 
 class King(Piece) :
-    pass
+    def move(self, new_pos) :
+        flag=False
+        for i in range(-1,2) :
+            for j in range(-1,2) :
+                if (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1] +j):
+                    self.pos = new_pos
+                    flag=True
+                    break
+            if (flag==True):
+                    break
 
 class Knight(Piece) :
-    pass
+    def move(self, new_pos) :
+        if ((new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]+2) or (new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]-2) or (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]+1) or (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]-1) or(new_pos[0]==self.pos[0]-2 and new_pos[1] == self.pos[1]+1) or (new_pos[0] == self.pos[0]-2 and new_pos[1] == self.pos[1]-1) or (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]+2) or (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]-2) ):
+            self.pos = new_pos
     
