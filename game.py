59ee36b4e4f9a99e1745632bb9aee4a6a63ef209
Houssam BEN OTHMAN <houssam.ben_othman@edu.devinci.fr>
2026-05-19 13:03:10 +0200

    Ajout de detection de checkmate,stalemate,draw,resign,end turn , playturn Pour les bon déroulé de la partie

--- game.py
+++ game.py
@@ -76,12 +76,79 @@ class game:
         self.board.board[6][6] = bpawn7
         self.board.board[7][6] = bpawn8
 
-        self.wplayer1.pieces = [wrook1,wknight1,wbishop1,wqueen,wking,wbishop2,wknight2,wrook2,wpawn1,wpawn2,wpawn3,wpawn4,wpawn5,wpawn6,wpawn7,wpawn8]
-        self.bplayer2.pieces = [brook1,bknight1,bbishop1,bqueen,bking,bbishop2,bknight2,brook2,bpawn1,bpawn2,bpawn3,bpawn4,bpawn5,bpawn6,bpawn7,bpawn8]
-    
+        self.wplayer1.pieces = [wking,wrook1,wknight1,wbishop1,wqueen,wbishop2,wknight2,wrook2,wpawn1,wpawn2,wpawn3,wpawn4,wpawn5,wpawn6,wpawn7,wpawn8]
+        self.bplayer2.pieces = [bking,brook1,bknight1,bbishop1,bqueen,bbishop2,bknight2,brook2,bpawn1,bpawn2,bpawn3,bpawn4,bpawn5,bpawn6,bpawn7,bpawn8]
+
     def eat_piece(self, piece): #to remove a piece from the board and from the player's pieces when it's eaten by the opponent
         if piece.color == "white":
             self.wplayer1.sup_piece(piece)
         else:
             self.bplayer2.sup_piece(piece)
     
+    def checkmate(self, player): #to check if the player is in checkmate
+        if player.color == "white":
+            opponent_pieces = self.bplayer2.pieces
+            king_pos = self.wplayer1.pieces[0].pos
+        else:
+            opponent_pieces = self.wplayer1.pieces
+            king_pos = self.bplayer2.pieces[0].pos
+        
+        for piece in opponent_pieces:
+            if piece.can_move(king_pos):
+                return True
+        return False
+    
+    def stalemate(self, player): #to check if the player is in stalemate
+        if player.color == "white":
+            opponent_pieces = self.bplayer2.pieces
+            king_pos = self.wplayer1.pieces[0].pos
+        else:
+            opponent_pieces = self.wplayer1.pieces
+            king_pos = self.bplayer2.pieces[0].pos
+        
+        for piece in opponent_pieces:
+            if piece.can_move(king_pos):
+                return False
+        return True
+    
+    def draw(self): #to check if the game is a draw
+        if len(self.wplayer1.pieces) == 1 and len(self.bplayer2.pieces) == 1:
+            return True
+        return False
+    
+    def resign(self, player,resign=False): #to allow the player to resign
+        if player.color == "white" and resign==True:
+            return "black wins"
+        elif player.color == "black" and resign==True:
+            return "white wins" 
+    
+    def end_game(self, player,resign=False): #to end the game when a player is in checkmate, stalemate, draw or resigns
+        if self.checkmate(player):
+            if player.color == "white":
+                return "black wins"
+            else:
+                return "white wins"
+        elif self.stalemate(player):
+            return "stalemate"
+        elif self.draw():
+            return "draw"
+        elif resign==True:
+            return self.resign(player,resign)
+        
+    def end_turn(self, player, endturn=False): #to end the turn for the player and start the timer for the opponent
+        if player.color == "white" and endturn==True:
+            self.bplayer2.timer(self.bplayer2.time)
+        elif player.color == "black" and endturn==True:
+            self.wplayer1.timer(self.wplayer1.time)
+    
+    def play_turn(self, player, piece, new_pos):
+        if piece.color != player.color:
+            return "invalid move"
+        old_pos = piece.pos
+        if self.board.Taken_box(new_pos):
+            self.eat_piece(self.board.ReturnPiece(new_pos))
+        piece.move(new_pos)
+        self.board.board[new_pos[0]][new_pos[1]] = piece
+        self.board.board[old_pos[0]][old_pos[1]] = 0
+        self.end_turn(player, endturn=True)
+        return self.end_game(player)
