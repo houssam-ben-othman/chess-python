8885aa5998233a0b911aab3ca6ff43f4d67504b4
Houssam BEN OTHMAN <houssam.ben_othman@edu.devinci.fr>
2026-05-19 12:31:14 +0200

    Ajout de méthode indiquant quelle piece sur une position et si la case indiquer possède une pièce ou est vide

--- board.py
+++ board.py
@@ -2,4 +2,11 @@ class Board:
     def __init__(self):
         self.board = [[0 for _ in range(8)] for _ in range(8)]
 
+    def Taken_box(self, pos): #to check if the box is taken or not
+        if self.board[pos[0]][pos[1]] != 0:
+            return True
+        else:
+            return False
     
+    def ReturnPiece(self, pos): #to return the piece in the given position
+        return self.board[pos[0]][pos[1]]
