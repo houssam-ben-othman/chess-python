8b9566b698ca7ed3e15e1563615ab53504212435
Houssam BEN OTHMAN <houssam.ben_othman@edu.devinci.fr>
2026-07-12 11:48:57 +0200

    Ajout du timer et de manger en diagonale pour un pion

--- piece.py
+++ piece.py
@@ -1,114 +1,140 @@
 class Piece :
-    def __init__(self, color,pos) :
+    def __init__(self, color, pos) :
         self.color = color
         self.pos = pos
     def move(self, new_pos) :
         self.pos = new_pos
     def can_move(self, new_pos) :
         return False
-    
+
 class Pawn(Piece) :
-        def move(self, new_pos) : #to move the pawn one or two steps forward if it's the first move, one step forward otherwise, or one step diagonally to capture
-        if self.color == "white" :
-            if ( self.pos[1]==1) :
-               if (new_pos[1] == self.pos[1] + 1 and new_pos[0] == self.pos[0] or (new_pos[1] == self.pos[1] + 2 and new_pos[0] == self.pos[0])) :
-            else :
-                if (new_pos[1] == self.pos[1] + 1 and new_pos[0] == self.pos[0]) :
-                    self.pos = new_pos
-            if (new_pos[1] == self.pos[1] + 1 and (new_pos[0] == self.pos[0] + 1 or new_pos[0] == self.pos[0] - 1)) :
-                self.pos = new_pos
-        else :
-            if ( self.pos[1]==6) :
-               if (new_pos[1] == self.pos[1] - 1 and new_pos[0] == self.pos[0] or (new_pos[1] == self.pos[1] - 2 and new_pos[0] == self.pos[0])) :
-            else :
-                if (new_pos[1] == self.pos[1] - 1 and new_pos[0] == self.pos[0]) :
-                    self.pos = new_pos
-            if (new_pos[1] == self.pos[1] - 1 and (new_pos[0] == self.pos[0] + 1 or new_pos[0] == self.pos[0] - 1)) :
-                self.pos = new_pos
+    def move_piece(self, player, start_pos, end_pos):
+        piece = self.board.ReturnPiece(start_pos)
+        if piece == 0:
+            return False
+        if piece.color != player.color:
+            return False
+        if self.board.Taken_box(end_pos) and self.board.ReturnPiece(end_pos).color == player.color:
+            return False
+        if type(piece).__name__ == "Pawn":
+            if end_pos[0] == start_pos[0]:
+                if not piece.can_move(end_pos):
+                    return False
+                if self.board.Taken_box(end_pos):
+                    return False
+            else:
+                if not self.board.Taken_box(end_pos):
+                    return False
+                if piece.color == "white":
+                    if not (end_pos[1] == start_pos[1] + 1 and (end_pos[0] == start_pos[0] + 1 or end_pos[0] == start_pos[0] - 1)):
+                        return False
+                else:
+                    if not (end_pos[1] == start_pos[1] - 1 and (end_pos[0] == start_pos[0] + 1 or end_pos[0] == start_pos[0] - 1)):
+                        return False
+        else:
+            if not piece.can_move(end_pos):
+                return False
+        resultat = self.play_turn(player, piece, end_pos)
+        if resultat is not None:
+            return resultat
+        return True
 
     def can_move(self, new_pos) :
         if self.color == "white" :
-            if ( self.pos[1]==1) :
+            if (self.pos[1] == 1) :
+                if (new_pos[1] == self.pos[1] + 1 and new_pos[0] == self.pos[0] or (new_pos[1] == self.pos[1] + 2 and new_pos[0] == self.pos[0])) :
+                    return True
             else :
                 if (new_pos[1] == self.pos[1] + 1 and new_pos[0] == self.pos[0]) :
                     return True
             if (new_pos[1] == self.pos[1] + 1 and (new_pos[0] == self.pos[0] + 1 or new_pos[0] == self.pos[0] - 1)) :
                 return True
         else :
-            if ( self.pos[1]==6) :
-               if (new_pos[1] == self.pos[1] - 1 and new_pos[0] == self.pos[0] or (new_pos[1] == self.pos[1] - 2 and new_pos[0] == self.pos[0])) :
+            if (self.pos[1] == 6) :
+                if (new_pos[1] == self.pos[1] - 1 and new_pos[0] == self.pos[0] or (new_pos[1] == self.pos[1] - 2 and new_pos[0] == self.pos[0])) :
+                    return True
             else :
                 if (new_pos[1] == self.pos[1] - 1 and new_pos[0] == self.pos[0]) :
                     return True
             if (new_pos[1] == self.pos[1] - 1 and (new_pos[0] == self.pos[0] + 1 or new_pos[0] == self.pos[0] - 1)) :
                 return True
         return False
-class Rook(Piece) : #to move the rook in a straight line either horizontally or vertically
+
+class Rook(Piece) :
     def move(self, new_pos) :
         if ((new_pos[0] == self.pos[0] or new_pos[1] == self.pos[1]) and new_pos != self.pos) :
             self.pos = new_pos
-
     def can_move(self, new_pos) :
         if ((new_pos[0] == self.pos[0] or new_pos[1] == self.pos[1]) and new_pos != self.pos) :
             return True
         return False
 
-class Bishop(Piece) : #to move the bishop in a straight line diagonally
+class Bishop(Piece) :
     def move(self, new_pos) :
-        for i in range(-7,8) :
-           if i != 0 :
-                if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i )or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
-                    self.pos = new_pos
-
-    def can_move(self, new_pos) :
-        for i in range(-7,8) :
+        for i in range(-7, 8) :
             if i != 0 :
-                if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i )or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
+                if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i) or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
+                    self.pos = new_pos
+    def can_move(self, new_pos) :
+        for i in range(-7, 8) :
+            if i != 0 :
+                if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i) or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
                     return True
         return False
 
-class Queen(Piece) : #to move the queen in a straight line either horizontally, vertically or diagonally
+class Queen(Piece) :
     def can_move(self, new_pos) :
         if (new_pos[0] == self.pos[0] or new_pos[1] == self.pos[1]) and new_pos != self.pos :
             return True
         else :
-            for i in range(-7,8) :
+            for i in range(-7, 8) :
                 if i != 0 :
-                    if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i )or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
+                    if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i) or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
                         return True
         return False
-    
     def move(self, new_pos) :
         if (new_pos[0] == self.pos[0] or new_pos[1] == self.pos[1]) and new_pos != self.pos :
             self.pos = new_pos
         else :
-            for i in range(-7,8) :
+            for i in range(-7, 8) :
                 if i != 0 :
-                    if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i )or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
+                    if ((new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+i) or (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]-i)) :
                         self.pos = new_pos
 
-class King(Piece) : #to move the king one step in any direction
+class King(Piece) :
     def move(self, new_pos) :
-        for i in range(-1,2) :
-            for j in range(-1,2) :
+        for i in range(-1, 2) :
+            for j in range(-1, 2) :
                 if not (i == 0 and j == 0) :
-                    if (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1] +j):
+                    if (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+j) :
                         self.pos = new_pos
-    
     def can_move(self, new_pos) :
-        for i in range(-1,2) :
-            for j in range(-1,2) :
+        for i in range(-1, 2) :
+            for j in range(-1, 2) :
                 if not (i == 0 and j == 0) :
-                    if (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1] +j):
+                    if (new_pos[0] == self.pos[0]+i and new_pos[1] == self.pos[1]+j) :
                         return True
         return False
 
-class Knight(Piece) : #to move the knight in an L shape
+class Knight(Piece) :
     def move(self, new_pos) :
-        if ((new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]+2) or (new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]-2) or (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]+1) or (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]-1) or(new_pos[0]==self.pos[0]-2 and new_pos[1] == self.pos[1]+1) or (new_pos[0] == self.pos[0]-2 and new_pos[1] == self.pos[1]-1) or (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]+2) or (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]-2) ):
+        if ((new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]+2) or
+            (new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]-2) or
+            (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]+1) or
+            (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]-1) or
+            (new_pos[0] == self.pos[0]-2 and new_pos[1] == self.pos[1]+1) or
+            (new_pos[0] == self.pos[0]-2 and new_pos[1] == self.pos[1]-1) or
+            (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]+2) or
+            (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]-2)) :
             self.pos = new_pos
-    
     def can_move(self, new_pos) :
-        if ((new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]+2) or (new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]-2) or (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]+1) or (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]-1) or(new_pos[0]==self.pos[0]-2 and new_pos[1] == self.pos[1]+1) or (new_pos[0] == self.pos[0]-2 and new_pos[1] == self.pos[1]-1) or (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]+2) or (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]-2) ):
+        if ((new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]+2) or
+            (new_pos[0] == self.pos[0]+1 and new_pos[1] == self.pos[1]-2) or
+            (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]+1) or
+            (new_pos[0] == self.pos[0]+2 and new_pos[1] == self.pos[1]-1) or
+            (new_pos[0] == self.pos[0]-2 and new_pos[1] == self.pos[1]+1) or
+            (new_pos[0] == self.pos[0]-2 and new_pos[1] == self.pos[1]-1) or
+            (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]+2) or
+            (new_pos[0] == self.pos[0]-1 and new_pos[1] == self.pos[1]-2)) :
             return True
         return False
--- ui.py
+++ ui.py
@@ -58,24 +58,47 @@ def draw_pieces(board):
                 pygame_x, pygame_y = board_to_screen((col, row))
                 screen.blit(texte, (pygame_x + 15, pygame_y + 10))
 
-def draw_info(message, tour):
+def format_timer(secondes) :
+    minutes = int(secondes) // 60
+    secs = int(secondes) % 60
+    return f"{minutes:02}:{secs:02}"
+
+def draw_info(message, tour, temps_blanc, temps_noir) :
     pygame.draw.rect(screen, (30, 30, 30), (0, 800, 800, 100))
-    if tour == "white":
+    if tour == "white" :
         couleur_tour = (255, 255, 255)
         texte_tour = font_info.render("Tour : Blanc", True, couleur_tour)
-    else:
+    else :
         couleur_tour = (180, 180, 180)
         texte_tour = font_info.render("Tour : Noir", True, couleur_tour)
-    screen.blit(texte_tour, (20, 815))
-    if message != "":
+    screen.blit(texte_tour, (20, 810))
+    if temps_blanc <= 30 :
+        couleur_blanc = (255, 80, 80)
+    else :
+        couleur_blanc = (255, 255, 255)
+    if temps_noir <= 30 :
+        couleur_noir = (255, 80, 80)
+    else :
+        couleur_noir = (180, 180, 180)
+    texte_blanc = font_info.render("Blanc : " + format_timer(temps_blanc), True, couleur_blanc)
+    texte_noir = font_info.render("Noir : " + format_timer(temps_noir), True, couleur_noir)
+    screen.blit(texte_blanc, (20, 840))
+    screen.blit(texte_noir, (250, 840))
+    if message != "" :
         texte_msg = font_info.render(message, True, (255, 220, 50))
-        screen.blit(texte_msg, (20, 850))
+        screen.blit(texte_msg, (500, 840))
 
 def setup_players():
     white_player = Player("Joueur 1", "white", 600)
     black_player = Player("Joueur 2", "black", 600)
     return white_player, black_player
 
+
+
+
+
+
+# Main loop of the game
 white_player, black_player = setup_players()
 b = Board()
 chess_game = game(b, white_player, black_player)
@@ -83,53 +106,80 @@ chess_game.start()
 
 selected_pos = None
 message = ""
+temps_blanc = 600.0
+temps_noir = 600.0
+temps_debut_tour = time.time()
 continuer = True
 
-while continuer:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+while continuer :
+    temps_ecoule = time.time() - temps_debut_tour
+
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
             continuer = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN and chess_game.endgame == False:
+        if event.type == pygame.MOUSEBUTTONDOWN and chess_game.endgame == False :
             pixel_x, pixel_y = event.pos
-            if pixel_y < 800:
+            if pixel_y < 800 :
                 clicked_pos = screen_to_board(pixel_x, pixel_y)
 
-                if selected_pos is None:
+                if selected_pos is None :
                     piece = b.ReturnPiece(clicked_pos)
-                    if piece != 0 and piece.color == chess_game.turn:
+                    if piece != 0 and piece.color == chess_game.turn :
                         selected_pos = clicked_pos
                         message = ""
-                else:
-                    if chess_game.turn == "white":
+                else :
+                    if chess_game.turn == "white" :
                         current_player = white_player
-                    else:
+                    else :
                         current_player = black_player
 
                     result = chess_game.move_piece(current_player, selected_pos, clicked_pos)
-                    if result == True:
+                    if result == True :
                         message = "Coup valide !"
-                    elif result == False:
+                        if current_player.color == "white" :
+                            temps_blanc -= temps_ecoule
+                        else :
+                            temps_noir -= temps_ecoule
+                        temps_debut_tour = time.time()
+                    elif result == False :
                         message = "Coup invalide, reessayez."
-                    elif result == "black wins":
-                        message = "Les noirs gagnent ! Partie terminee."
+                    elif result == "black wins" :
+                        message = "Les noirs gagnent !"
                         chess_game.endgame = True
-                    elif result == "white wins":
-                        message = "Les blancs gagnent ! Partie terminee."
+                    elif result == "white wins" :
+                        message = "Les blancs gagnent !"
                         chess_game.endgame = True
-                    elif result == "draw":
+                    elif result == "draw" :
                         message = "Match nul !"
                         chess_game.endgame = True
                     selected_pos = None
 
+    if chess_game.endgame == False :
+        if chess_game.turn == "white" :
+            temps_blanc_affiche = max(0, temps_blanc - temps_ecoule)
+            temps_noir_affiche = max(0, temps_noir)
+        else :
+            temps_blanc_affiche = max(0, temps_blanc)
+            temps_noir_affiche = max(0, temps_noir - temps_ecoule)
+        if temps_blanc_affiche == 0 :
+            message = "Temps ecoule ! Les noirs gagnent."
+            chess_game.endgame = True
+        if temps_noir_affiche == 0 :
+            message = "Temps ecoule ! Les blancs gagnent."
+            chess_game.endgame = True
+    else :
+        temps_blanc_affiche = max(0, temps_blanc)
+        temps_noir_affiche = max(0, temps_noir)
+
     screen.fill((0, 0, 0))
     draw_board()
 
-    if selected_pos is not None:
+    if selected_pos is not None :
         draw_highlight(selected_pos, (50, 200, 50))
 
     draw_pieces(b)
-    draw_info(message, chess_game.turn)
+    draw_info(message, chess_game.turn, temps_blanc_affiche, temps_noir_affiche)
     pygame.display.flip()
 
 pygame.quit()
